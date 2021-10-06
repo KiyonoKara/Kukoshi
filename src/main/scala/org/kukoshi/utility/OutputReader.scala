7962d5b81423f891ed5953c54c46f932792c31d9
Yen <68307546+KaNguy@users.noreply.github.com>
2021-10-06 08:45:20 -0700

    Fix: Remove unnecessary `if` statement in while loop.

--- src/main/scala/org/kukoshi/utility/OutputReader.scala
+++ src/main/scala/org/kukoshi/utility/OutputReader.scala
@@ -38,9 +38,6 @@ object OutputReader {
     // Appending the data to a String Builder
     while (ch != -1) {
       ch = reader.read()
-      if (ch == -1) {
-        return stringBuilder.toString()
-      }
       stringBuilder.append(ch.asInstanceOf[Char]).toString
     }
     stringBuilder.toString
