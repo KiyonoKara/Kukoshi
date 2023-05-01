51491452361a0a1f20344e65e52aa314152970ac
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-05-01 03:47:26 -0400

    Fix: remove null and add task for detecting compression

--- src/main/scala/org/kukoshi/utility/OutputReader.scala
+++ src/main/scala/org/kukoshi/utility/OutputReader.scala
@@ -3,37 +3,37 @@ package org.kukoshi.utility
 import java.io.{InputStream, InputStreamReader, Reader}
 import java.net.HttpURLConnection
 import java.util.zip.{DeflaterInputStream, GZIPInputStream}
+import scala.collection.mutable.{StringBuilder => StrBuilder}
 
 object OutputReader {
   /**
    * Reads output of a connection established via the HttpURLConnection class
    *
    * @param connection  HttpURLConnection
-   * @param inputStream InputStream
    * @return String of the output
    */
-  def read(connection: HttpURLConnection, inputStream: InputStream = null): String = {
-    var connectionInputStream: InputStream = null
-    if (inputStream != null) connectionInputStream = inputStream else connectionInputStream = connection.getInputStream
-
+  def read(connection: HttpURLConnection): String = {
+    val connectionInputStream: InputStream = connection.getInputStream
     // Set the reader to a null value before reading the output
-    var reader: Reader = null
+    var reader: Reader = new InputStreamReader(connection.getInputStream)
 
     // GZIP & Deflate data streaming
+    // TODO: correct this by checking the headers set by the program
+    /*
     if (connection.getContentEncoding != null && connection.getContentEncoding.nonEmpty) {
       connection.getContentEncoding match {
         case "gzip" => reader = new InputStreamReader(new GZIPInputStream(connectionInputStream))
         case "deflate" => reader = new InputStreamReader(new DeflaterInputStream(connectionInputStream))
         case _ => reader = new InputStreamReader(connection.getInputStream)
       }
-    } else reader = new InputStreamReader(connection.getInputStream)
-
+    }*/
 
     // Empty char value
     var ch: Int = 0
 
     // String Builder to add to the final string
-    val stringBuilder: StringBuilder = new StringBuilder()
+    // StringBuilder => StrBuilder
+    val stringBuilder: StrBuilder = new StrBuilder()
 
     // Appending the data to a String Builder
     while (ch != -1) {
