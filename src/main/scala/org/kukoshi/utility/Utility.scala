9411baa2d02134391a22a43a95f06688144f87b4
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-12-09 17:05:59 -0500

    Removal: Remove getKeyByValue and lowerCaseSingleKV due library not requiring it.

--- src/main/scala/org/kukoshi/utility/Utility.scala
+++ src/main/scala/org/kukoshi/utility/Utility.scala
@@ -32,33 +32,6 @@ object Utility {
     s"""$newURL$separator$encodedURLParameters"""
   }
 
-  /**
-   * Gets a map key by its value
-   * @param map   Collection map
-   * @param value Value of the key
-   * @return Key name
-   */
-  def getKeyByValue(map: Map[String, String] = Map.empty[String, String], value: String): String = {
-    map.find(_._2.contains(value)).map(_._1).getOrElse("")
-  }
-
-  /**
-   * Manipulates the key and value, and returns it all in lower-case (this is not recommended for API key headers)
-   *
-   * @param map Map collection
-   * @param key A valid key from the map
-   * @return A new map that just has the lower-cased data
-   */
-  def lowerCaseSingleKV(map: Map[String, String] = Map.empty[String, String], key: String): Map[String, String] = {
-    val fin: Map[String, String] = Map.empty[String, String]
-    map.foreach(item => {
-      if (item._1 == key) {
-        fin updated(item._1.toLowerCase, item._2.toLowerCase)
-      }
-    })
-    fin
-  }
-
   def setHeaders(connection: HttpURLConnection, headers: Iterable[(String, String)] = Nil): Unit = {
     if (headers.nonEmpty) {
       headers foreach {
