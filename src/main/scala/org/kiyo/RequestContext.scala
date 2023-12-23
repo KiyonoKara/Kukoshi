5a1c17a991ce247759aec7d5f4d11ac9a9b2531a
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-12-22 19:00:27 -0800

    Abstraction: RequestContext for holding data

---
+++ src/main/scala/org/kiyo/RequestContext.scala
@@ -0,0 +1,21 @@
+package org.kiyo
+
+import Utility.Constants
+
+/**
+ * Request context for abstracted request functions
+ * @param url            The URL
+ * @param method         The request method
+ * @param hasBody        Whether request has a body to send
+ * @param data           The data for the body
+ * @param headers        Request headers
+ * @param readTimeout    Request reading timeout
+ * @param connectTimeout Request connection timeout
+ */
+protected case class RequestContext(url: String,
+                                    method: String = Constants.GET,
+                                    hasBody: Boolean = true,
+                                    data: String = new String(),
+                                    headers: Iterable[(String, String)] = Iterable.empty[(String, String)],
+                                    readTimeout: Int,
+                                    connectTimeout: Int)
