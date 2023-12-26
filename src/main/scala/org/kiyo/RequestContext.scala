5afe1ae39362cde14023cd4d6e76b184672c6ab4
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-12-25 21:03:48 -0700

    Doc: Update clarity for hasBody

--- src/main/scala/org/kiyo/RequestContext.scala
+++ src/main/scala/org/kiyo/RequestContext.scala
@@ -11,7 +11,7 @@ import org.kiyo.Constants
  * Request context for abstracted request functions
  * @param url            The URL
  * @param method         The request method
- * @param hasBody        Whether request has a body to send
+ * @param hasBody        If the request has data to send
  * @param data           The data for the body
  * @param headers        Request headers
  * @param readTimeout    Request reading timeout
