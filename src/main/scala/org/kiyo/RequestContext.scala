29887769de94628a28b8ae255c3825bbe48b1758
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-12-27 15:56:29 -0700

    Feature: RequestContext may specify if it will send a payload and / or receive a payload

--- src/main/scala/org/kiyo/RequestContext.scala
+++ src/main/scala/org/kiyo/RequestContext.scala
@@ -2,6 +2,8 @@ package org.kiyo
 
 import org.kiyo.Constants
 
+import scala.annotation.targetName
+
 /**
  * Created 2023/12/22
  * File RequestContext.scala
@@ -9,17 +11,19 @@ import org.kiyo.Constants
 
 /**
  * Request context for abstracted request functions
- * @param url            The URL
- * @param method         The request method
- * @param hasBody        If the request has data to send
- * @param data           The data for the body
- * @param headers        Request headers
- * @param readTimeout    Request reading timeout
- * @param connectTimeout Request connection timeout
+ * @param url                The URL
+ * @param method             The request method
+ * @param `sendsPayload?`    If the request has data to send
+ * @param `receivesPayload?` If the response expects a payload
+ * @param data               The data for the body
+ * @param headers            Request headers
+ * @param readTimeout        Request reading timeout
+ * @param connectTimeout     Request connection timeout
  */
 protected case class RequestContext(url: String,
                                     method: String = Constants.GET,
-                                    hasBody: Boolean = true,
+                                    @targetName("sendsPayload") `sendsPayload?`: Boolean = false,
+                                    @targetName("receivesPayload") `receivesPayload?`: Boolean = true,
                                     data: String = new String(),
                                     headers: Iterable[(String, String)] = Iterable.empty,
                                     readTimeout: Int,
