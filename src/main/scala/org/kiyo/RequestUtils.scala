16b7540bcf769c04a5ced47487999b6ff07ed9c7
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-12-28 16:34:24 -0700

    Refactor: Refactor to RequestUtils and data reading

--- src/main/scala/org/kiyo/RequestUtils.scala
+++ src/main/scala/org/kiyo/RequestUtils.scala
@@ -5,9 +5,12 @@ package org.kiyo
  * File RequestUtils.scala
  */
 
+import java.io.InputStream
 import java.net.{HttpURLConnection, URI, URL, URLEncoder}
 import java.util
-import scala.jdk.CollectionConverters.*
+import java.util.zip.{GZIPInputStream, InflaterInputStream}
+import scala.collection.mutable
+import scala.jdk.CollectionConverters.{ListHasAsScala, MapHasAsScala}
 
 /**
  * Utility object
@@ -45,6 +48,58 @@ object RequestUtils {
       strBuilder.append("%s: %s%n".format(entry._1, entry._2.mkString(",")))
     )
     strBuilder.toString
+  }
+
+  /**
+   * Reads output of a connection established from HttpURLConnection
+   * @param connection HttpURLConnection
+   * @return Data as a String
+   */
+  def readConnectionData(connection: HttpURLConnection): String = {
+    val connectionInputStream: InputStream = connection.getInputStream
+
+    // Get all headers and set all to lowercase
+    val headerFields: mutable.Map[String, List[String]] = connection.getHeaderFields.asScala
+      .filter(_._1 != null)
+      .map((k, v) => (k.toLowerCase, v.asScala.toList))
+
+    // GZIP & Deflate data streaming
+    var compression: String = new String()
+    if (headerFields.getOrElse("content-encoding", List.empty).nonEmpty) {
+      val co_encoding: Seq[String] = headerFields.get("content-encoding").toSeq.flatten
+      compression = co_encoding.head
+    }
+
+    this.decodeAndRead(connectionInputStream, compression)
+  }
+
+  /**
+   * Uses a supplied input stream and specified content encoding and decodes accordingly
+   * If the content encoding type isn't support, it will default to the supplied input stream
+   * @param inputStream     The input stream
+   * @param contentEncoding The type of content encoding (gzip or deflate)
+   * @return The (decoded) content
+   */
+  def decodeAndRead(inputStream: InputStream, contentEncoding: String): String = {
+    var reader: InputStream = inputStream
+
+    // Determine whether content encoding is gzip or deflate
+    contentEncoding match {
+      case "gzip" => reader = new GZIPInputStream(inputStream)
+      case "deflate" => reader = new InflaterInputStream(inputStream)
+      case _ => ()
+    }
+
+    // Start with empty char value
+    var ch: Int = 0
+    val stringBuilder: StringBuilder = new StringBuilder()
+
+    // Appends each character from the data to the StringBuilder
+    while (ch != -1) {
+      ch = reader.read()
+      stringBuilder.append(ch.toChar)
+    }
+    stringBuilder.toString
   }
 
   /**
